374f31870c7b89d56c77510acb4d25a21febabaa
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2024-11-21 23:16:47 +0300

    .

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View>
+        <Text>Another text</Text>
+      </View>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title='press me' />
     </View>
   );
 }
